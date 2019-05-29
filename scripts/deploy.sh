b788c36d75720c2c225162ed7a5296edfb76b736
Eugeniy <drobovik45@yandex.ru>
2019-05-29 17:19:12 +0300

    first

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,14 +5,14 @@ mvn clean package
 echo 'Copy files...'
 
 scp -i ~/.ssh/id_rsa_drucoder \
-    target/sweater-1.0-SNAPSHOT.jar \
-    dru@192.168.0.107:/home/dru/
+    target/tweater-1.1-SNAPSHOT.jar \
+    drobovik@192.168.112.128:/home/drobovik/
 
 echo 'Restart server...'
 
 ssh -i ~/.ssh/id_rsa_drucoder drobovik@192.168.112.128 << EOF
 pgrep java | xargs kill -9
-nohup java -jar sweater-1.0-SNAPSHOT.jar > log.txt &
+nohup java -jar tweater-1.1-SNAPSHOT.jar > log.txt &
 EOF
 
 echo 'Bye'
